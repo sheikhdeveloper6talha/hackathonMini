56568d59f1a22b3445d9110a8bb88021d7040e44
Muhammad Talha Sheikh <sheikhdeveloper6@gmail.com>
2026-04-19 13:59:43 +0500

    Update leadership.js

--- public/components/leadership.js
+++ public/components/leadership.js
@@ -1,6 +1,38 @@
 const menuBtn = document.querySelector(".menu-btn");
 const navLinks = document.querySelector(".nav-links");
 
-menuBtn.addEventListener("click",function(){
+menuBtn.addEventListener("click", function(){
 navLinks.classList.toggle("active");
+
+
+});
+
+// Animation div hero Selection
+
+gsap.registerPlugin(ScrollTrigger);
+
+gsap.from(".cardleft", {
+  opacity: 0,
+  x: -150,
+  duration: 1,
+  stagger: 0.4,
+
+  scrollTrigger: {
+    trigger: ".main-grid",
+    start: "top 80%",
+    toggleActions: "restart none restart none"
+  }
+});
+
+gsap.from(".cardRight", {
+  opacity: 0,
+  x: 50,
+  duration: 1,
+  stagger: 0.4,
+
+  scrollTrigger: {
+    trigger: ".main-grid",
+    start: "top 80%",
+    toggleActions: "restart none restart none"
+  }
 });
