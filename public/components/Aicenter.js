6f44667bb5b24b41fda17f845fd8dab5ca02132f
Muhammad Talha Sheikh <sheikhdeveloper6@gmail.com>
2026-04-19 13:57:06 +0500

    Update Aicenter.js

--- public/components/Aicenter.js
+++ public/components/Aicenter.js
@@ -2,3 +2,21 @@ function toggleMenu(){
 const navLinks = document.getElementById("navLinks");
 navLinks.classList.toggle("active");
 }
+
+
+// Animation div hero Selection
+
+gsap.registerPlugin(ScrollTrigger);
+
+gsap.from(".hero-section", {
+  opacity: 0,
+  y: -350,
+  duration: 1,
+  stagger: 0.4,
+
+  scrollTrigger: {
+    trigger: ".container",
+    start: "top 80%",
+    toggleActions: "restart none restart none"
+  }
+});
