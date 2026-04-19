a531f2905943166e375c5073e012649b30b99a19
Muhammad Talha Sheikh <sheikhdeveloper6@gmail.com>
2026-04-19 13:58:21 +0500

    Update explore.js

--- public/components/explore.js
+++ public/components/explore.js
@@ -3,4 +3,48 @@ const navLinks = document.querySelector(".nav-links");
 
 menuBtn.addEventListener("click", function(){
 navLinks.classList.toggle("active");
+console.log('working');
+
+});
+
+
+// Animation div hero Selection
+
+gsap.registerPlugin(ScrollTrigger);
+
+gsap.from(".hero-header", {
+  opacity: 0,
+  y: -150,
+  duration: 1,
+  stagger: 0.4,
+
+  scrollTrigger: {
+    trigger: ".container",
+    start: "top 80%",
+    toggleActions: "restart none restart none"
+  }
+});
+gsap.from(".filters-card", {
+  opacity: 0,
+  x: -150,
+  duration: 1,
+  stagger: 0.4,
+
+  scrollTrigger: {
+    trigger: ".explore-grid",
+    start: "top 80%",
+    toggleActions: "restart none restart none"
+  }
+});
+gsap.from(".req-card", {
+  opacity: 0,
+  x: 150,
+  duration: 1,
+  stagger: 1,
+
+  scrollTrigger: {
+    trigger: ".explore-grid",
+    start: "top 80%",
+    toggleActions: "restart none restart none"
+  }
 });
