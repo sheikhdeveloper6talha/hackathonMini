58eb97e1e4b32c1baef338f777eb4b452c7527d7
Muhammad Talha Sheikh <sheikhdeveloper6@gmail.com>
2026-04-19 14:14:19 +0500

    Update createRequest.js

--- public/components/pages/createRequest.js
+++ public/components/pages/createRequest.js
@@ -5,10 +5,49 @@ menuBtn.addEventListener("click", function(){
 
 navLinks.classList.toggle("active");
 
-if(navLinks.classList.contains("active")){
-menuBtn.innerHTML="✖";
-}else{
-menuBtn.innerHTML="☰";
-}
+});
 
+// Animation div hero Selection
+
+gsap.registerPlugin(ScrollTrigger);
+
+gsap.from(".hero-card", {
+  opacity: 0,
+  y: -250,
+  duration: 1,
+  stagger: 0.4,
+
+  scrollTrigger: {
+    trigger: ".container",
+    start: "top 80%",
+    toggleActions: "restart none restart none"
+  }
+});
+// Animation div hero Selection
+
+gsap.registerPlugin(ScrollTrigger);
+
+gsap.from(".left-col", {
+  opacity: 0,
+  x: -250,
+  duration: 1,
+  stagger: 0.4,
+
+  scrollTrigger: {
+    trigger: ".detail-grid",
+    start: "top 80%",
+    toggleActions: "restart none restart none"
+  }
+});
+gsap.from(".right-col", {
+  opacity: 0,
+  x: 750,
+  duration: 1,
+  stagger: 0.4,
+
+  scrollTrigger: {
+    trigger: ".detail-grid",
+    start: "top 80%",
+    toggleActions: "restart none restart none"
+  }
 });
